cc4a24ec77bd270415d483ad4cf5c2116043b864
a5892731 <a5892731@o2.pl>
2021-11-01 17:08:21 +0100

    2021-11-01 17:03   - add file browser func for many files

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from pdf2image import convert_from_path
 import os
 
 class File:
-    def __init__(self, filename, input_path = "pdf", output_path = 'png', input_file_type = ".pdf"):
+    def __init__(self, filename, input_path, output_path, input_file_type):
         self.size_x = 0
         self.size_y = 0
         self.dpi_size = 500
@@ -19,10 +19,15 @@ class File:
         self.pages = None
         self.poppler_path = r"C:\Users\a5892\poppler-21.10.0\Library\bin"   # red about this path in READ_ME file!
 
+
+class PdfFile(File):
+    def __init__(self, filename, input_path = "pdf", output_path = 'png', input_file_type = ".pdf"):
+        super().__init__(filename, input_path = "pdf", output_path = 'png', input_file_type = ".pdf")
+
         self.import_pdf_file_pages()
         self.convert_pdf_to_png(filename)
 
-class PdfFile(File):
+
     def import_pdf_file_pages(self):
         self.pages = convert_from_path(pdf_path=self.file_path, dpi= self.dpi_size,
                                        poppler_path=self.poppler_path, timeout=600
